f00ca8034368b9780f4eaaff81f8602e11a23e3a
Linus Ahlenius <linus.ahlenius@gmail.com>
2021-10-30 23:58:16 +0200

    Sample location continuously

--- app/src/main/java/dev/ahlenius/beenthere/MainActivity.kt
+++ app/src/main/java/dev/ahlenius/beenthere/MainActivity.kt
@@ -9,7 +9,6 @@ import android.location.LocationManager
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
@@ -17,17 +16,19 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.MarkerOptions
-import dev.ahlenius.beenthere.databinding.ActivityMainBinding
 import com.google.android.gms.maps.model.LatLng
+import dev.ahlenius.beenthere.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    private var lastLocation: Location = createDummyLocation()
+    private var lastLocation: Location = Location("").apply {
+        latitude = 59.3
+        longitude = 18.9
+    }
 
-    lateinit var map: GoogleMap
-    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     lateinit var binding: ActivityMainBinding
+    private lateinit var map: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,34 +42,44 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             R.id.map_fragment
         ) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        startLocationUpdates()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        if (hasLocationPermissions()) {
+            if (isLocationEnabled()) {
+                fusedLocationProviderClient.requestLocationUpdates(
+                    buildLocationRequest(),
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            }
+        } else {
+            requestLocationPermissions()
+            startLocationUpdates()
+        }
+
+
     }
 
     @SuppressLint("MissingPermission")
     override fun onMapReady(map: GoogleMap) {
-        val stockholmPosition = LatLng(59.334591, 18.063240)
-        getLastLocation()
         this.map = map
         map.moveCamera(
             CameraUpdateFactory.newLatLngZoom(
                 LatLng(
-                    this.lastLocation.latitude,
-                    this.lastLocation.longitude
+                    lastLocation.latitude,
+                    lastLocation.longitude
                 ),
                 6.toFloat()
             )
         )
-        map.addMarker(MarkerOptions().position(stockholmPosition))
         map.isMyLocationEnabled = true
     }
 
-    private fun createDummyLocation(): Location {
-        val location = Location("Dummy")
-        location.latitude = 0.0
-        location.longitude = 0.0
-        return location
-    }
-
-    fun hasLocationPermissions(): Boolean {
+    private fun hasLocationPermissions(): Boolean {
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_COARSE_LOCATION
@@ -84,7 +95,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         return false
     }
 
-    fun requestLocationPermissions() {
+    private fun requestLocationPermissions() {
         ActivityCompat.requestPermissions(
             this,
             arrayOf(
@@ -95,56 +106,30 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         )
     }
 
-    fun isLocationEnabled(): Boolean {
+    private fun isLocationEnabled(): Boolean {
         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
-            LocationManager.NETWORK_PROVIDER
-        )
-    }
-
-    @SuppressLint("MissingPermission", "SetTextI18n")
-    fun getLastLocation() {
-        if (hasLocationPermissions()) {
-            if (isLocationEnabled()) {
-                fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
-                    val location: Location? = task.result
-                    if (location == null) {
-                        newLocationData()
-                    } else {
-                        Log.d("Debug:", "Long ${location.longitude}, Lat ${location.latitude}")
-                        binding.locationText.text =
-                            "Long: ${location.longitude}, Lat: ${location.latitude}"
-                        this.lastLocation = location
-                    }
-                }
-            } else {
-                Toast.makeText(this, "Please Turn on Your device Location", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        } else {
-            requestLocationPermissions()
-        }
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
     }
 
     @SuppressLint("MissingPermission")
-    fun newLocationData() {
+    fun buildLocationRequest(): LocationRequest {
         val locationRequest = LocationRequest.create()
         locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        locationRequest.interval = 0
-        locationRequest.fastestInterval = 0
-        locationRequest.numUpdates = 1
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        fusedLocationProviderClient.requestLocationUpdates(
-            locationRequest, locationCallback, Looper.myLooper()
-        )
+        locationRequest.interval = 5000
+        locationRequest.fastestInterval = 3000
+        locationRequest.smallestDisplacement = 1F
+        return locationRequest
     }
-
 
     private val locationCallback = object : LocationCallback() {
         @SuppressLint("SetTextI18n")
         override fun onLocationResult(locationResult: LocationResult) {
-            val lastLocation: Location = locationResult.lastLocation
-            Log.d("Debug:", "Last location: " + lastLocation.longitude.toString())
+            lastLocation = locationResult.lastLocation
+            Log.d(
+                "Debug:",
+                "New Location - Long: ${lastLocation.longitude}, Lat: ${lastLocation.latitude}"
+            )
             binding.locationText.text =
                 "Long: ${lastLocation.longitude} , Lat: ${lastLocation.latitude}"
         }
